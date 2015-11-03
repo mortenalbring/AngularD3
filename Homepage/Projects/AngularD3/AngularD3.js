a304f0a5eae8bf631a152f5f7e3eaac3c4203d53
Morten Albring <mortenalbring@gmail.com>
2015-11-03 20:54:51 +0000

    Drawing graph with data from array

--- Homepage/Projects/AngularD3/AngularD3.js
+++ Homepage/Projects/AngularD3/AngularD3.js
@@ -1,152 +1,117 @@
-// Define the dimensions of the visualization. We're using
-// a size that's convenient for displaying the graphic on
-// http://jsDataV.is
+var scope = {};
 
-var width = 640,
-    height = 480;
+scope.graph =
+{
+    width: 400,
+    height: 200,
+    margin: 30,
+    radius: 3,
+    svg: null,
+    force: null,
+    data: {
+        nodes: [{ID:1, Name: "Test Node 1"},
+            {ID: 2, Name: 'Test Node 2'}],
+        edges: [{StartNode: 1, EndNode: 2}]
+    }
+}
 
-// Define the data for the example. In general, a force layout
-// requires two data arrays. The first array, here named `nodes`,
-// contains the object that are the focal point of the visualization.
-// The second array, called `links` below, identifies all the links
-// between the nodes. (The more mathematical term is "edges.")
 
-// For the simplest possible example we only define two nodes. As
-// far as D3 is concerned, nodes are arbitrary objects. Normally the
-// objects wouldn't be initialized with `x` and `y` properties like
-// we're doing below. When those properties are present, they tell
-// D3 where to place the nodes before the force layout starts its
-// magic. More typically, they're left out of the nodes and D3 picks
-// random locations for each node. We're defining them here so we can
-// get a consistent application of the layout which lets us see the
-// effects of different properties.
+function drawGraph() {
 
-var nodes = [
-    { x:   width/3, y: height/2 },
-    { x: 2*width/3, y: height/2 }
-];
+    d3.select("svg").remove();
 
-// The `links` array contains objects with a `source` and a `target`
-// property. The values of those properties are the indices in
-// the `nodes` array of the two endpoints of the link.
+    scope.graph.data.linkData = linksIndexes(scope.graph.data.edges, scope.graph.data.nodes);
 
-var links = [
-    { source: 0, target: 1 }
-];
+    scope.graph.svg = d3.select('#graph-container').append('svg')
+        .attr('width', scope.graph.width)
+        .attr('height', scope.graph.height)
+        .attr('class', 'svg-container');
 
-// Here's were the code begins. We start off by creating an SVG
-// container to hold the visualization. We only need to specify
-// the dimensions for this container.
+    //This sets up the parameters for the force directed graph simulation
+    scope.graph.force = d3.layout.force()
+        .size([scope.graph.width - scope.graph.margin, scope.graph.height - scope.graph.margin])
+        .nodes(scope.graph.data.nodes)
+        .links(scope.graph.data.linkData)
+        .linkDistance(50)
+        //Distance between nodes
+        .charge(-400)
+        //How much nodes repel eachother (positive is attractive)
+        .friction(0.5)
+        //Slows down the nodes movement
+        .gravity(0.25)
+        //An attractive force towards the centre of the graph
+        .on("tick", tick);
 
-var svg = d3.select('body').append('svg')
-    .attr('width', width)
-    .attr('height', height);
+    scope.graph.force.start();
 
-// Now we create a force layout object and define its properties.
-// Those include the dimensions of the visualization and the arrays
-// of nodes and links.
 
-var force = d3.layout.force()
-    .size([width, height])
-    .nodes(nodes)
-    .links(links);
+    scope.graph.linklines = scope.graph.svg.selectAll(".link");
+    scope.graph.data.linkData = linksIndexes(scope.graph.data.edges, scope.graph.data.nodes);
 
-// There's one more property of the layout we need to define,
-// its `linkDistance`. That's generally a configurable value and,
-// for a first example, we'd normally leave it at its default.
-// Unfortunately, the default value results in a visualization
-// that's not especially clear. This parameter defines the
-// distance (normally in pixels) that we'd like to have between
-// nodes that are connected. (It is, thus, the length we'd
-// like our links to have.)
+    scope.graph.linklines = scope.graph.linklines.data(scope.graph.data.linkData, function (d) {
+        return d.source.ID + "-" + d.target.ID;
+    });
 
-force.linkDistance(width/2);
+    scope.graph.linklines.enter()
+        .insert("line", ".node")
+        .attr("class", function (e) {
+            return "link link-default";
+        });
+    scope.graph.linklines.exit().remove();
 
-// Next we'll add the nodes and links to the visualization.
-// Note that we're just sticking them into the SVG container
-// at this point. We start with the links. The order here is
-// important because we want the nodes to appear "on top of"
-// the links. SVG doesn't really have a convenient equivalent
-// to HTML's `z-index`; instead it relies on the order of the
-// elements in the markup. By adding the nodes _after_ the
-// links we ensure that nodes appear on top of links.
+    //Container for both the node and the label describing the node
+    var gnodes = scope.graph.svg.selectAll('g.gnode')
+        .data(scope.graph.data.nodes)
+        .enter()
+        .append('g')
+        .attr("class", function (d) {
+            return "node-container";
+        })
+        .classed('gnode', true)
+        .on("click", function (d) {
+            console.log(d);
+        })
+        .call(scope.graph.force.drag);
 
-// Links are pretty simple. They're just SVG lines, and
-// we're not even going to specify their coordinates. (We'll
-// let the force layout take care of that.) Without any
-// coordinates, the lines won't even be visible, but the
-// markup will be sitting inside the SVG container ready
-// and waiting for the force layout.
+    var cnode = gnodes.append("circle")
+        .attr("class", "cnode")
+        .attr("r", 2);
 
-var link = svg.selectAll('.link')
-    .data(links)
-    .enter().append('line')
-    .attr('class', 'link');
+    var labels = gnodes.append("text")
+        .attr("class", "label-text")
+        .text(function (d) { return d.Name });
 
-// Now it's the nodes turn. Each node is drawn as a circle.
 
-var node = svg.selectAll('.node')
-    .data(nodes)
-    .enter().append('circle')
-    .attr('class', 'node');
+    function linksIndexes(dbEdges, dbNodes) {
+        //The array of nodes and edges must be transformed into an object
+        //that contains the actual node objects as source and target
+        var output = [];
+        for (var i = 0; i < dbEdges.length; i++) {
+            var startId = dbEdges[i].StartNode;
+            var endId = dbEdges[i].EndNode;
+            var sourceNode = dbNodes.filter(function (n) { return n.ID == startId; });
+            var targetNode = dbNodes.filter(function (n) { return n.ID == endId; });
 
-// We're about to tell the force layout to start its
-// calculations. We do, however, want to know when those
-// calculations are complete, so before we kick things off
-// we'll define a function that we want the layout to call
-// once the calculations are done.
+            if ((sourceNode.length == 1) && (targetNode.length == 1)) {
+                output.push({
+                    source: sourceNode[0],
+                    target: targetNode[0]
+                });
+            }
+        }
+        return output;
+    }
 
-force.on('end', function() {
+    function tick() {
+        gnodes.attr("transform", function (d) {
+            return 'translate(' + [d.x, d.y] + ')';
+        });
 
-    // When this function executes, the force layout
-    // calculations have concluded. The layout will
-    // have set various properties in our nodes and
-    // links objects that we can use to position them
-    // within the SVG container.
+        scope.graph.linklines.attr("x1", function (d) { return d.source.x; })
+            .attr("y1", function (d) { return d.source.y; })
+            .attr("x2", function (d) { return d.target.x; })
+            .attr("y2", function (d) { return d.target.y; });
+    }
+}
 
-    // First let's reposition the nodes. As the force
-    // layout runs it updates the `x` and `y` properties
-    // that define where the node should be centered.
-    // To move the node, we set the appropriate SVG
-    // attributes to their new values. We also have to
-    // give the node a non-zero radius so that it's visible
-    // in the container.
-
-    node.attr('r', width/25)
-        .attr('cx', function(d) { return d.x; })
-        .attr('cy', function(d) { return d.y; });
-
-    // We also need to update positions of the links.
-    // For those elements, the force layout sets the
-    // `source` and `target` properties, specifying
-    // `x` and `y` values in each case.
-
-    link.attr('x1', function(d) { return d.source.x; })
-        .attr('y1', function(d) { return d.source.y; })
-        .attr('x2', function(d) { return d.target.x; })
-        .attr('y2', function(d) { return d.target.y; });
-
-});
-
-// Okay, everything is set up now so it's time to turn
-// things over to the force layout. Here we go.
-
-force.start();
-
-// By the time you've read this far in the code, the force
-// layout has undoubtedly finished its work. Unless something
-// went horribly wrong, you should see two light grey circles
-// connected by a single dark grey line. If you have a screen
-// ruler (such as [xScope](http://xscopeapp.com) handy, measure
-// the distance between the centers of the two circles. It
-// should be somewhere close to the `linkDistance` parameter we
-// set way up in the beginning (480 pixels). That, in the most
-// basic of all nutshells, is what a force layout does. We
-// tell it how far apart we want connected nodes to be, and
-// the layout keeps moving the nodes around until they get
-// reasonably close to that value.
-
-// Of course, there's quite a bit more than that going on
-// under the hood. We'll take a closer look starting with
-// the next example.
-
+drawGraph();
