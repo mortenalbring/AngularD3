8da113eec5674da6ab555c2555ae48494b2c2816
Morten Albring <mortenalbring@gmail.com>
2016-01-10 14:10:57 +0000

    Removed backups

--- Homepage/Projects/AngularD3/AngularD3.js.BACKUP.9196.js
+++
@@ -1,4 +0,0 @@
-var angularD3 = angular.module("angularD3", ["angularD3Controllers"]);
-
-angularD3.service('SettingsService', AngularD3SettingsService);
-angularD3.service('GraphService',['$rootScope','SettingsService', AngularD3GraphService]);
